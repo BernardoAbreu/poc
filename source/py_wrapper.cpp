cfa0b0aa206b3a7ac8b0e4d3ccec506b4a697a57
bernardoabreu <bernardoaabreu@gmail.com>
2018-09-12 18:10:48 -0300

    Add wrapper for python

---
+++ source/py_wrapper.cpp
@@ -0,0 +1,98 @@
+//  Copyright Joel de Guzman 2002-2004. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//  Hello World Example from the tutorial
+//  [Joel de Guzman 10/9/2002]
+
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/list.hpp>
+#include <boost/python/extract.hpp>
+#include <boost/python/tuple.hpp>
+#include <stdexcept>
+#include <vector>
+#include <list>
+
+#include "process.h"
+
+
+template<typename T>
+using Matrix = std::vector<std::vector<T> >;
+
+
+
+void obtain_patterns(const Matrix<mol_info> &points, std::list<Pattern> &out, int k){
+    Graph g;
+
+    build_graph(g, points, k);
+
+    level1(g, out);
+
+    post_process(out);
+}
+
+
+void build_matrix_from_python(PyObject* incoming, Matrix<mol_info> &data){
+    if (PyList_Check(incoming)) {
+        data.resize((int)PyList_Size(incoming));
+
+        for(Py_ssize_t i = 0; i < PyList_Size(incoming); i++) {
+            PyObject *row = PyList_GetItem(incoming, i);
+
+            for(Py_ssize_t j = 0; j < PyList_Size(row); j++) {
+                PyObject *value = PyList_GetItem(row, j);
+                std::pair<int, double> p = std::pair<int, double>(PyFloat_AsDouble(PyTuple_GetItem(value, 0)), PyFloat_AsDouble(PyTuple_GetItem(value, 1)));
+                data[i].push_back(p);
+            }
+        }
+    } else {
+        throw std::logic_error("Passed PyObject pointer was not a list!");
+    }
+}
+
+
+void build_out_matrix(const std::list<Pattern> &out, boost::python::list &mols, boost::python::list &all_points){
+    for(auto &pat : out){
+        boost::python::list new_mols, new_points;
+
+        for(auto &mol : pat.molecules){
+            new_mols.append(mol);
+        }
+        mols.append(new_mols);
+
+        for(auto &points : pat.points){
+            new_points.append(points);
+        }
+        all_points.append(new_points);
+    }
+}
+
+
+boost::python::tuple mine_patterns(PyObject* incoming){
+    Matrix<mol_info> points;
+    std::list<Pattern> out;
+
+    build_matrix_from_python(incoming, points);
+
+    obtain_patterns(points, out, 1);
+
+    boost::python::list mols, all_points;
+
+    build_out_matrix(out, mols, all_points);
+    return boost::python::make_tuple(mols, all_points);
+}
+
+
+char const* greet()
+{
+   return "hello, world";
+}
+
+BOOST_PYTHON_MODULE(patmin)
+{
+    using namespace boost::python;
+    def("greet", greet);
+    // def("create", create);
+    def("mine_patterns", mine_patterns);
+}
+
