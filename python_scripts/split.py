6eb5089d034bd88893626993b2d47801bc8957a7
bernardoabreu <bernardoaabreu@gmail.com>
2019-01-18 16:15:02 +0100

    Change split.py to use string instead of numbers

--- python_scripts/split.py
+++ python_scripts/split.py
@@ -5,13 +5,11 @@ from sys import argv
 
 
 def split(input_file):
-    a = np.loadtxt(input_file)
-
-    a = np.loadtxt(input_file)
+    a = np.loadtxt(input_file, dtype=str)
 
     coulomb, lennard = np.split(a, 2, axis=1)
-    np.savetxt('T_C_' + input_file.split('/')[-1], coulomb.T)
-    np.savetxt('T_L_' + input_file.split('/')[-1], lennard.T)
+    np.savetxt('T_C_' + input_file.split('/')[-1], coulomb.T, fmt='%s')
+    np.savetxt('T_L_' + input_file.split('/')[-1], lennard.T, fmt='%s')
 
 
 if __name__ == '__main__':
